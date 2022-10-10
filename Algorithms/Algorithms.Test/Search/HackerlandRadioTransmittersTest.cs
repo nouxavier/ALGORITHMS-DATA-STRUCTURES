2e45efa4f535272e0d1ae332cc11e123e6f77c37
nouxavier <nouara.xavier@unico.io>
2022-10-10 14:45:46 -0300

    Adding challenge Hackland Radio Transmitters

---
+++ Algorithms/Algorithms.Test/Search/HackerlandRadioTransmittersTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Algorithms.Search;
+using ExpectedObjects;
+
+namespace Algorithms.Test.Search
+{
+    public class HackerlandRadioTransmittersTest
+    {
+        public HackerlandRadioTransmittersTest()
+        {
+        }
+
+        [Fact]
+        public void InstallRadio()
+        {
+            List<int> input = new List<int> { 7, 2, 4, 6, 5, 9, 12, 11 };
+            int k = 2;
+            var output = HackerlandRadioTransmitters.InstallRadio(input, k);
+            Assert.Equal(3, output);
+
+        }
+    }
+}
+
---
+++ Algorithms/Algorithms/Search/HackerlandRadioTransmitters.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace Algorithms.Search
+{
+    public class HackerlandRadioTransmitters
+    {
+        public HackerlandRadioTransmitters()
+        {
+        }
+
+        public static int InstallRadio(List<int> houses, int k)
+        {
+            var j = 0;
+            
+           
+            for (var i = 1; i < houses.Count; i++)
+            {
+                var value = houses[i];
+                j = i - 1;
+                while (j >= 0 && value < houses[j])
+                {
+                    houses[j + 1] = houses[j];
+                    j = j - 1;
+                }
+                houses[j + 1] = value;
+
+            }
+
+            
+            int max = houses[houses.Count-1];
+            int[] aux = new int[max];
+            j = 1;
+            for (int i=0; i< max; i++)
+            {
+                aux[i] = j;
+                j++;
+            }
+
+            int mid = 0;
+            int install = 0;
+            int end = 0;
+            List<int> aux2 = new List<int>();
+            for(int i = 0; i<houses.Count; i++)
+            {
+                mid = houses[i] + k;
+                //get local install
+                while(i < houses.Count && mid >= houses[i])
+                {
+                    i++;
+                    
+                }
+                aux2.Add(houses[i-1]);
+
+                if(i < houses.Count)
+                {
+                    //get max distance install
+                    end = houses[i - 1] + k;
+                    while (i < houses.Count && end >= houses[i])
+                    {
+                        i++;
+
+                    }
+                    i = i - 1;
+                }
+                install++;
+            }
+
+            return install;
+        }
+    }
+
+
+}
+
