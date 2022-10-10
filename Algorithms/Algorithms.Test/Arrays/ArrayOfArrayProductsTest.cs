a5191754c6a6bdcdc1e65e4a17b01cc75d7864d1
nouxavier <nouara.xavier@unico.io>
2022-10-10 17:01:56 -0300

    adding challenge Array of Array Product

---
+++ Algorithms/Algorithms.Test/Arrays/ArrayOfArrayProductsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Algorithms.Arrays;
+
+namespace Algorithms.Test.Arrays
+{
+    public class ArrayOfArrayProductsTest
+    {
+        public ArrayOfArrayProductsTest()
+        {
+        }
+
+        [Fact]
+        public void ProductSuccess()
+        {
+
+            var input = new int[] { -3, 17, 430, -6, 5, -12, -11, 5 };
+            var expect = new long[] { -144738000, 25542000, 1009800, -72369000, 86842800, -36184500, -39474000, 86842800 };
+            var result = ArrayOfArrayProducts.Product(input);
+            Assert.Equal(expect, result);
+        }
+
+        [Fact]
+        public void Product2Success()
+        {
+
+            var input = new int[] { 2, 7, 3, 4 };
+            var expect = new long[] { 84, 24, 56, 42 };
+            var result = ArrayOfArrayProducts.ProductGood(input);
+            Assert.Equal(expect, result);
+        }
+    }
+}
+
---
+++ Algorithms/Algorithms/Arrays/ArrayOfArrayProducts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Algorithms.Arrays
+{
+    public class ArrayOfArrayProducts
+    {
+        public ArrayOfArrayProducts()
+        {
+        }
+      
+
+        public static long[]Product(int[] arr)
+        {
+            long[] answer = new long[arr.Length];
+
+            if (arr.Length <= 1)
+                return answer;
+           
+            for(int i = 0; i< arr.Length; i++)
+            {
+                answer[i] = 1;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if(j != i)
+                    {
+                        var teste = answer[i] * arr[j];
+                        answer[i] = answer[i] * arr[j];
+                    }
+                }
+            }
+
+            return answer;
+
+        }
+        public static long[] ProductGood(int[] arr)
+        {
+            long[] repoIndexProduct = new long[arr.Length];
+
+            if (arr.Length <= 1)
+                return repoIndexProduct;
+
+            var product = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                repoIndexProduct[i] = product;
+                product = product * arr[i];
+                
+            }
+            product = 1;
+            for (int i = arr.Length-1; i >=0; i--)
+            {
+                repoIndexProduct[i] = repoIndexProduct[i] * product;
+                product = product * arr[i];
+
+            }
+
+            return repoIndexProduct;
+
+        }
+    }
+}
+
